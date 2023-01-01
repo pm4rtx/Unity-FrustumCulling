6fb85394e2b535ce8b47d324b951aa9f506d32f9
pm4rtx <pm4rtx@gmail.com>
2023-01-01 15:47:37 +0900

    Suppresses NASM output and removes /verbose flag from link/lib commands

--- Scripts/frustumcull.build.cmd
+++ Scripts/frustumcull.build.cmd
@@ -9,38 +9,38 @@ if not exist .Build mkdir .Build
 pushd .Build
 
 :: a dummy .obj file with DLL entry point
-nasm -f win64 -Oxv ../Sources/%file_name%.asm -o %file_name%_null.obj -l %file_name%_null.lst
+nasm -fwin64 -Oxv ../Sources/%file_name%.asm -o %file_name%_null.obj -l %file_name%_null.lst > nul
 
 :: SSE2 version
 set fname=%file_name%_aosoa4_aabb_stream_simd4_sse2
-nasm -DFUNCTION_NAME=%fname% -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm
+nasm -DFUNCTION_NAME=%fname% -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm > nul
 
 :: AVX version with 256-bit registers (VEX.256). The expected input is the stream of aosoa8 = {float8 minx,miny,minz,maxx,maxy,maxz } structures
 set fname=%file_name%_aosoa8_aabb_stream_simd8_avx
-nasm -DFUNCTION_NAME=%fname% -DUSE_AVX=1 -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm
+nasm -DFUNCTION_NAME=%fname% -DUSE_AVX=1 -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm > nul
 
 :: AVX version with 128-bit registers (VEX.128). The expected input is the stream of aosoa4 = {float8 minx,miny,minz,maxx,maxy,maxz } structures
 set fname=%file_name%_aosoa4_aabb_stream_simd4_avx
-nasm -DFUNCTION_NAME=%fname% -DUSE_AVX=1 -DSIMD_SIZE=4 -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm
+nasm -DFUNCTION_NAME=%fname% -DUSE_AVX=1 -DSIMD_SIZE=4 -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm > nul
 
 :: AVX version with 256-bit registers (VEX.256). The expected input is the stream of aosoa4 = {float8 minx,miny,minz,maxx,maxy,maxz } structures
 set fname=%file_name%_aosoa4_aabb_stream_simd8_avx
-nasm -DFUNCTION_NAME=%fname% -DUSE_AVX=1 -DUSE_AOSOA4_INPUT=1 -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm
+nasm -DFUNCTION_NAME=%fname% -DUSE_AVX=1 -DUSE_AOSOA4_INPUT=1 -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm > nul
 
 :: all versions same as above but with extra test against the bounding box of the frustum which removes some false positives
 set fname=%file_name%_extratests_aosoa4_aabb_stream_simd4_sse2
-nasm -DFUNCTION_NAME=%fname% -DUSE_EXTRATESTS=1 -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm
+nasm -DFUNCTION_NAME=%fname% -DUSE_EXTRATESTS=1 -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm > nul
 
 set fname=%file_name%_extratests_aosoa8_aabb_stream_simd8_avx
-nasm -DFUNCTION_NAME=%fname% -DUSE_EXTRATESTS=1 -DUSE_AVX=1 -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm
+nasm -DFUNCTION_NAME=%fname% -DUSE_EXTRATESTS=1 -DUSE_AVX=1 -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm > nul
 
 set fname=%file_name%_extratests_aosoa4_aabb_stream_simd4_avx
-nasm -DFUNCTION_NAME=%fname% -DUSE_EXTRATESTS=1 -DUSE_AVX=1 -DSIMD_SIZE=4 -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm
+nasm -DFUNCTION_NAME=%fname% -DUSE_EXTRATESTS=1 -DUSE_AVX=1 -DSIMD_SIZE=4 -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm > nul
 
 set fname=%file_name%_extratests_aosoa4_aabb_stream_simd8_avx
-nasm -DFUNCTION_NAME=%fname% -DUSE_EXTRATESTS=1 -DUSE_AVX=1 -DUSE_AOSOA4_INPUT=1 -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm
+nasm -DFUNCTION_NAME=%fname% -DUSE_EXTRATESTS=1 -DUSE_AVX=1 -DUSE_AOSOA4_INPUT=1 -fwin64 -Oxv -o %fname%.obj -l %fname%.lst ../Sources/%file_name%.asm > nul
 
-lib /wx /nologo /subsystem:windows /machine:x64 /verbose /out:lib%file_name%.lib^
+lib /wx /nologo /subsystem:windows /machine:x64 /out:lib%file_name%.lib^
     %file_name%_aosoa4_aabb_stream_simd4_sse2.obj^
     %file_name%_aosoa8_aabb_stream_simd8_avx.obj^
     %file_name%_aosoa4_aabb_stream_simd4_avx.obj^
@@ -50,7 +50,7 @@ lib /wx /nologo /subsystem:windows /machine:x64 /verbose /out:lib%file_name%.lib
     %file_name%_extratests_aosoa4_aabb_stream_simd4_avx.obj^
     %file_name%_extratests_aosoa4_aabb_stream_simd8_avx.obj
 
-link /nologo /opt:ref /dll /map /LARGEADDRESSAWARE /entry:_DllMainCRTStartup /machine:X64 /verbose /pdbstripped:%file_name%.pdb /out:%file_name%.dll^
+link /nologo /opt:ref /dll /map /LARGEADDRESSAWARE /entry:_DllMainCRTStartup /machine:x64 /pdbstripped:%file_name%.pdb /out:%file_name%.dll^
     %file_name%_aosoa4_aabb_stream_simd4_sse2.obj^
     %file_name%_aosoa8_aabb_stream_simd8_avx.obj^
     %file_name%_aosoa4_aabb_stream_simd4_avx.obj^
--- Scripts/frustumcull.make
+++ Scripts/frustumcull.make
@@ -2,38 +2,40 @@ BUILD_DIR = .Build
 SOURCE_DIR = Sources
 FILE_NAME = frustumcull
 
+.silent:
+
 # a dummy .obj file with DLL entry point
 $(BUILD_DIR)\$(FILE_NAME)_null.obj: $(SOURCE_DIR)\$(FILE_NAME).asm
-    nasm -fwin64 -Oxv -o $*.obj -l $*.lst $?
+    nasm -fwin64 -Oxv -o $*.obj -l $*.lst $? > nul
 
 # SSE2 version
 $(BUILD_DIR)\$(FILE_NAME)_aosoa4_aabb_stream_simd4_sse2.obj: $(SOURCE_DIR)\$(FILE_NAME).asm
-    nasm -DFUNCTION_NAME=$(*B) -fwin64 -Ox -o$*.obj -l $*.lst $?
+    nasm -DFUNCTION_NAME=$(*B) -fwin64 -Ox -o$*.obj -l $*.lst $? > nul
 
 # AVX version with 256-bit registers (VEX.256). The expected input is the stream of aosoa8 = {float8 minx,miny,minz,maxx,maxy,maxz } structures
 $(BUILD_DIR)\$(FILE_NAME)_aosoa8_aabb_stream_simd8_avx.obj: $(SOURCE_DIR)\$(FILE_NAME).asm
-    nasm -DFUNCTION_NAME=$(*B) -DUSE_AVX=1 -fwin64 -Ox -o $*.obj -l $*.lst $?
+    nasm -DFUNCTION_NAME=$(*B) -DUSE_AVX=1 -fwin64 -Ox -o $*.obj -l $*.lst $? > nul
 
 # AVX version with 128-bit registers (VEX.128). The expected input is the stream of aosoa4 = {float8 minx,miny,minz,maxx,maxy,maxz } structures
 $(BUILD_DIR)\$(FILE_NAME)_aosoa4_aabb_stream_simd4_avx.obj: $(SOURCE_DIR)\$(FILE_NAME).asm
-    nasm -DFUNCTION_NAME=$(*B) -DUSE_AVX=1 -DSIMD_SIZE=4 -fwin64 -Ox -o $*.obj -l $*.lst $?
+    nasm -DFUNCTION_NAME=$(*B) -DUSE_AVX=1 -DSIMD_SIZE=4 -fwin64 -Ox -o $*.obj -l $*.lst $? > nul
 
 # AVX version with 256-bit registers (VEX.256). The expected input is the stream of aosoa4 = {float8 minx,miny,minz,maxx,maxy,maxz } structures
 $(BUILD_DIR)\$(FILE_NAME)_aosoa4_aabb_stream_simd8_avx.obj: $(SOURCE_DIR)\$(FILE_NAME).asm
-    nasm -DFUNCTION_NAME=$(*B) -DUSE_AVX=1 -DUSE_AOSOA4_INPUT=1 -fwin64 -Ox -o $*.obj -l $*.lst $?
+    nasm -DFUNCTION_NAME=$(*B) -DUSE_AVX=1 -DUSE_AOSOA4_INPUT=1 -fwin64 -Ox -o $*.obj -l $*.lst $? > nul
 
 # all versions same as above but with extra test against the bounding box of the frustum which removes some false positives
 $(BUILD_DIR)\$(FILE_NAME)_extratests_aosoa4_aabb_stream_simd4_sse2.obj: $(SOURCE_DIR)\$(FILE_NAME).asm
-    nasm -DFUNCTION_NAME=$(*B) -DUSE_EXTRATESTS=1 -fwin64 -Ox -o $*.obj -l $*.lst $?
+    nasm -DFUNCTION_NAME=$(*B) -DUSE_EXTRATESTS=1 -fwin64 -Ox -o $*.obj -l $*.lst $? > nul
 
 $(BUILD_DIR)\$(FILE_NAME)_extratests_aosoa8_aabb_stream_simd8_avx.obj: $(SOURCE_DIR)\$(FILE_NAME).asm
-    nasm -DFUNCTION_NAME=$(*B) -DUSE_EXTRATESTS=1 -DUSE_AVX=1 -fwin64 -Ox -o $*.obj -l $*.lst $?
+    nasm -DFUNCTION_NAME=$(*B) -DUSE_EXTRATESTS=1 -DUSE_AVX=1 -fwin64 -Ox -o $*.obj -l $*.lst $? > nul
 
 $(BUILD_DIR)\$(FILE_NAME)_extratests_aosoa4_aabb_stream_simd4_avx.obj: $(SOURCE_DIR)\$(FILE_NAME).asm
-    nasm -DFUNCTION_NAME=$(*B) -DUSE_EXTRATESTS=1 -DUSE_AVX=1 -DSIMD_SIZE=4 -fwin64 -Ox -o $*.obj -l $*.lst $?
+    nasm -DFUNCTION_NAME=$(*B) -DUSE_EXTRATESTS=1 -DUSE_AVX=1 -DSIMD_SIZE=4 -fwin64 -Ox -o $*.obj -l $*.lst $? > nul
 
 $(BUILD_DIR)\$(FILE_NAME)_extratests_aosoa4_aabb_stream_simd8_avx.obj: $(SOURCE_DIR)\$(FILE_NAME).asm
-    nasm -DFUNCTION_NAME=$(*B) -DUSE_EXTRATESTS=1 -DUSE_AVX=1 -DUSE_AOSOA4_INPUT=1 -fwin64 -Ox -o $*.obj -l $*.lst $?
+    nasm -DFUNCTION_NAME=$(*B) -DUSE_EXTRATESTS=1 -DUSE_AVX=1 -DUSE_AOSOA4_INPUT=1 -fwin64 -Ox -o $*.obj -l $*.lst $? > nul
 
 $(BUILD_DIR)\$(FILE_NAME).dll: \
     $(BUILD_DIR)\$(FILE_NAME)_aosoa4_aabb_stream_simd4_sse2.obj \
@@ -45,7 +47,7 @@ $(BUILD_DIR)\$(FILE_NAME).dll: \
     $(BUILD_DIR)\$(FILE_NAME)_extratests_aosoa4_aabb_stream_simd4_avx.obj \
     $(BUILD_DIR)\$(FILE_NAME)_extratests_aosoa4_aabb_stream_simd8_avx.obj \
     $(BUILD_DIR)\$(FILE_NAME)_null.obj
-    link /nologo /opt:ref /dll /map /LARGEADDRESSAWARE /entry:_DllMainCRTStartup /machine:x64 /verbose /pdbstripped:.$*.pdb /out:$*.dll $**
+    link /wx /nologo /opt:ref /dll /map /LARGEADDRESSAWARE /entry:_DllMainCRTStartup /machine:x64 /pdbstripped:.$*.pdb /out:$*.dll $**
 
 $(BUILD_DIR)\lib$(FILE_NAME).lib: \
     $(BUILD_DIR)\$(FILE_NAME)_aosoa4_aabb_stream_simd4_sse2.obj \
@@ -57,7 +59,7 @@ $(BUILD_DIR)\lib$(FILE_NAME).lib: \
     $(BUILD_DIR)\$(FILE_NAME)_extratests_aosoa4_aabb_stream_simd4_avx.obj \
     $(BUILD_DIR)\$(FILE_NAME)_extratests_aosoa4_aabb_stream_simd8_avx.obj \
     $(BUILD_DIR)\$(FILE_NAME)_null.obj
-    lib /wx /nologo /subsystem:windows /machine:x64 /verbose /out:$*.lib $**
+    lib /wx /nologo /subsystem:windows /machine:x64 /out:$*.lib $**
 
 mkdir:
     @if not exist $(BUILD_DIR) mkdir $(BUILD_DIR)
